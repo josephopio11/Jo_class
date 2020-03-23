867b5dd3a262a4629438f0425319feb50cca69d5
Joseph Opio <josephopio11@gmail.com>
2020-03-23 08:08:31 +0300

    Uploading all these files

--- tests2.py
+++ tests2.py
@@ -1,3 +1,26 @@
-statement = 'I love my family. They are the best'
+# statement = 'I love my family They are the best'
+#
+# print(statement.split()[3])
 
-print(statement.split()[1])
+class Calculator:
+    def addition(x, y):
+        add = x + y
+        print(add)
+
+    def subtraction(x, y):
+        sub = x - y
+        print(sub)
+
+    def multiplication(x, y):
+        mult = x * y
+        print(mult)
+
+    def division(x, y):
+        div = x / y
+        print(div)
+
+
+Calculator.addition(2, 3)
+Calculator.division(5,4)
+Calculator.multiplication(6,7)
+Calculator.subtraction(6,100)
