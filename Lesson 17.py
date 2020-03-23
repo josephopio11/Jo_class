ca74a37a9d89f6e24a2e3a3237c9f3f343d75035
Joseph Opio <josephopio11@gmail.com>
2020-03-23 09:24:23 +0300

    Uploading all these files

--- Lesson 17.py
+++ Lesson 17.py
@@ -1,1 +1,15 @@
+class Robot:
+    def __init__(self, name, color, weight):
+        self.name = name
+        self.color = color
+        self.weight = weight
 
+    def introduce_self(self):
+        print("Hi my name is " + self.name)
+
+
+bot_1 = Robot("Tom", "red", 30)
+bot_2 = Robot("Jerry", "blue", 40)
+
+bot_1.introduce_self()
+bot_2.introduce_self()
