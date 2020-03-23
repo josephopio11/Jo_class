062b3590556154a3c47cdb8c9fbaed8a598529a6
Joseph Opio <josephopio11@gmail.com>
2020-03-23 08:40:53 +0300

    Uploading all these files

---
+++ Lesson 17.py
@@ -0,0 +1,4 @@
+class Robot:
+    def introduce_self(self):
+        print("My name is " + self.name)
+
--- tests2.py
+++ tests2.py
@@ -2,25 +2,47 @@
 #
 # print(statement.split()[3])
 
-class Calculator:
-    def addition(x, y):
-        add = x + y
-        print(add)
+# class Calculator:
+#     def addition(x, y):
+#         add = x + y
+#         print(add)
+#
+#     def subtraction(x, y):
+#         sub = x - y
+#         print(sub)
+#
+#     def multiplication(x, y):
+#         mult = x * y
+#         print(mult)
+#
+#     def division(x, y):
+#         div = x / y
+#         print(div)
+#
+#
+# Calculator.addition(2, 3)
+# Calculator.division(5,4)
+# Calculator.multiplication(6,7)
+# Calculator.subtraction(6,100)
 
-    def subtraction(x, y):
-        sub = x - y
-        print(sub)
 
-    def multiplication(x, y):
-        mult = x * y
-        print(mult)
-
-    def division(x, y):
-        div = x / y
-        print(div)
+class Animal:
+    legs = int(input('Enter the number of legs'))
+    wings = int(input('Enter the number of wings'))
 
 
-Calculator.addition(2, 3)
-Calculator.division(5,4)
-Calculator.multiplication(6,7)
-Calculator.subtraction(6,100)
+class Car:
+    def turn_left(self):
+        pass
+
+    def turn_right(self):
+        pass
+
+    def accelerate(self):
+        pass
+
+    def slow_down(self):
+        pass
+
+    def open_window(self):
+        pass
